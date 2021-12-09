1dff87373d0d2e8da16e42fb3ddf89438e003e0e
Adam Obuchowicz <farmaazon@gmail.com>
2021-12-09 21:31:12 +0100

    Bump edition and versions. Make qmetaobject an optional dependency

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 mod game;
+
+#[cfg(feature = "qmetaobject")]
 mod qt_ui;
 
 pub use log;
 
 fn main() {
     simple_logger::SimpleLogger::new().init().unwrap();
+
+    #[cfg(feature = "qmetaobject")]
     qt_ui::main();
 }
