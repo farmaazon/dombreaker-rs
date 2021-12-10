92c35145f8b0e06ade2dc23284d263c546dd149f
Adam Obuchowicz <farmaazon@gmail.com>
2021-12-10 20:29:51 +0100

    More or less working

--- src/sixty_ui.rs
+++ src/sixty_ui.rs
@@ -8,7 +8,7 @@ use std::rc::Rc;
 
 sixtyfps::include_modules!();
 
-fn create_game_model(game: &Game) -> GameModel {
+fn create_dominoes_model(game: &Game) -> Rc<VecModel<DominoModel>> {
     let dominoes: Vec<DominoModel> = game
         .dominoes()
         .iter()
@@ -23,26 +23,22 @@ fn create_game_model(game: &Game) -> GameModel {
             horizontal: domino.orientation == Orientation::Horizontal,
         })
         .collect();
+    Rc::new(VecModel::from(dominoes))
+}
+
+fn create_game_model(game: &Game) -> GameModel {
+    let dominoes = create_dominoes_model(game);
     GameModel {
         board_width: game.board().width() as i32,
         board_height: game.board().height() as i32,
-        dominoes: ModelHandle::new(Rc::new(VecModel::from(dominoes))),
+        dominoes: ModelHandle::new(dominoes),
         score: game.score() as i32,
         finished: game.is_finished(),
     }
 }
 
-fn update_game_model(removed_dominoes: &[DominoRemoved], model: &mut GameModel) {
-    model.score += removed_dominoes
-        .iter()
-        .map(|d| d.score_awarded as i32)
-        .sum::<i32>();
+fn update_dominoes(removed_dominoes: &[DominoRemoved], dominoes: &VecModel<DominoModel>) {
     let removed_ids: HashSet<domino::Id> = removed_dominoes.iter().map(|d| d.id).collect();
-    let dominoes = model
-        .dominoes
-        .as_any()
-        .downcast_ref::<VecModel<DominoModel>>()
-        .unwrap();
     let mut i = 0;
     while i < dominoes.row_count() {
         let domino = dominoes.row_data(i);
@@ -52,6 +48,22 @@ fn update_game_model(removed_dominoes: &[DominoRemoved], model: &mut GameModel) 
             i += 1;
         }
     }
+}
+
+fn update_game_model(game: &Game, removed_dominoes: &[DominoRemoved], model: &mut GameModel) {
+    model.score += removed_dominoes
+        .iter()
+        .map(|d| d.score_awarded as i32)
+        .sum::<i32>();
+    model.finished = game.is_finished();
+    update_dominoes(
+        removed_dominoes,
+        model
+            .dominoes
+            .as_any()
+            .downcast_ref::<VecModel<DominoModel>>()
+            .unwrap(),
+    );
 }
 
 const LEVEL: &str = "--------\n\
@@ -64,7 +76,7 @@ const LEVEL: &str = "--------\n\
 
 pub fn main() {
     let main_window = Main::new();
-    let game = Game::new_generated(&LEVEL.repeat(50));
+    let game = Game::new_generated(&LEVEL);
     let model = create_game_model(&game);
     main_window.set_game(model);
 
@@ -74,7 +86,7 @@ pub fn main() {
         let main_window = weak.upgrade().unwrap();
         let result = game.borrow_mut().hit_domino(id as domino::Id);
         let mut model = main_window.get_game();
-        update_game_model(&result, &mut model);
+        update_game_model(&*game.borrow(), &result, &mut model);
         main_window.set_game(model);
     });
     main_window.run();
